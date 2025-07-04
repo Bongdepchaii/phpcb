e3158f4a163543548aee5211ed5b5f894bf48953
Bongdepchaii <Buitrongthanh2k5@gmail.com>
2025-07-04 08:01:48 +0700

    PHPcb

---
+++ b2.php
@@ -0,0 +1,24 @@
+<?php
+// define("PI", 3.14);
+// echo PI."\n";
+
+// const NAME = 'THANHBUI';
+// echo(NAME."\n");
+
+// $a = 10;
+// if ($a > 5){
+//     echo "a is greater than 5\n";
+// } else {
+//     echo "a is not greater than 5\n";
+// }
+
+$tuoi = 18;
+if($tuoi < 18 ){
+    echo "Ban chua du tuoi de lai xe may";
+} else if ($tuoi > 18 && $tuoi <= 60){
+    echo "Ban da du tuoi lai oto";
+} else {
+    echo "Ban da du tuoi lai xe may";
+}
+print_r($tuoi);
+?>
