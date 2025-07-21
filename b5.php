bcef60c5c38e9fc104bfcbfdb3e1241794f5294e
Bongdepchaii <Buitrongthanh2k5@gmail.com>
2025-07-21 07:25:51 +0700

    21/07/2025

---
+++ b5.php
@@ -0,0 +1,11 @@
+<?php
+// quy tuc dat tyen - coding convention
+// camelcase - bien ham phuong thuc
+// kebab-case - Ten dep, thuc muc
+// pascalcase - Lop, class, doi tuong
+// snake_case - Ten bien, ham, phuong thuc
+
+// Ham trong php
+// 1. dinh nghia ham
+
+?>
